e19e9d4ccc938b2cede543721e3c2eabc36f8978
Shorya Beohar <beohar77@gmail.com>
2024-12-08 18:46:38 +0530

    updated congig .ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -53,15 +53,15 @@ export default defineConfig({
         //storageState: "./playwright/.auth/auth.json" },
     }},
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    }
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // }
 
     /* Test against mobile viewports. */
     // {

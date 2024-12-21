b12a4dd656941d872e49ade9a377b057d2517d43
Shorya Beohar <beohar77@gmail.com>
2024-12-21 12:56:59 +0530

    TC

--- tests/VerifyTestCases.spec.ts
+++ tests/VerifyTestCases.spec.ts
@@ -1,11 +1,12 @@
-import {test,expect,page} from '../Fixtures/CustomFixtures'
+import {test,expect,page,context} from '../Fixtures/CustomFixtures'
 import {LoginDetails} from '../Testdata/Data.json'
 import  dotenv from "dotenv"
 import { Actions } from '../Util/Actions'
 import { logstep } from '../Util/AllurLogs'
+import { Assert } from '../Util/Assert'
 
 
-test.skip("Login to Application",{
+test("Login to Application",{
    annotation:{
        type: "Login in",
        description: "Test Case 2: verify TesT case page displayed"},
@@ -13,14 +14,13 @@ test.skip("Login to Application",{
 }, async ({pages,LoginFixture,LogoutFixture,browser}) => {
      
     await LoginFixture(LoginDetails.username,LoginDetails.password);
-    
-    logstep("Verify Test cases new page opened");
-        const context = await browser.newContext()
-        const page =  await context.newPage();
-        const contextPromise  = context.waitForEvent('page');
-        await Actions.clickElement(pages.objhomepage.btnTestCase);
-        const newpage = await contextPromise;
-        const val = await newpage.locator("//div[@class='panel-group']//h4");
-        expect((val)).toBe(27);
+    await Actions.wait(5000)
 
+    logstep("Verify Test cases new page opened");
+        await Actions.clickElement(pages.objhomepage.btnTestCase);
+        await Actions.wait(1000)
+        const  locTestCase= page.locator(".title b");
+        await Assert.expectToBeVisible(locTestCase);
+        const  TotalTestCase= page.locator(".panel-group");
+        await Assert.expectToHaveCount(TotalTestCase,27)
 })
